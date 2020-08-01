d114d1d70f2ea93b676de56d296eb98bea4f8199
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-01 15:44:38 +0200

    script changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,76 +3,76 @@ const width = 600;
 const height = 300;
 const margin = { top: 20, right: 0, bottom: 20, left: 20 };
 
-// Données
-const fruits = [
-  { name: "🍊", count: 21 , color: 'orange' },
-  { name: "🍇", count: 13 , color: 'blueviolet' },
-  { name: "🍏", count: 8 , color: 'greenyellow' },
-  { name: "🍌", count: 5 , color: 'yellow' },
-  { name: "🍐", count: 3 , color: 'yellowgreen' },
-  { name: "🍋", count: 2 , color: 'gold' },
-  { name: "🍎", count: 1 , color: 'red' },
-  { name: "🍉", count: 1 , color: 'orangered' }
-];
+d3.dsv(";", "Data/Donnees CP.csv", function (d) {
+    return {
+        annee : d.Year,
+        canton : d.Canton,
+        condamnations : d.CondamnationsAdultes
+    }
+}).then(function(Data) {
 
-// Créer l'élément SVG et le configurer
-const svg = d3.select('.main')
-              .append('svg')
-              .attr('width', width)
-              .attr('height', height)
-              .attr('style', 'font: 10px sans-serif')
+    console.log(Data)
+  
+  // Créer l'élément SVG et le configurer
+    const svg = d3.select('.main')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('style', 'font: 10px sans-serif')
+  
+  // Créer l'échelle horizontale (fonctions D3)
+    const x = d3.scaleBand()
+    .domain(canton.map(d => d.annee))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+    .round(true)
+  
+  // Créer l'échelle verticale (fonctions D3)
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(canton, d => d.condamnations)])
+    .range([height - margin.bottom - 5, margin.top])
+    .interpolate(d3.interpolateRound)
+  
+    const teinte = d3.scaleSequential()
+    .domain([0, d3.max(canton, d => d.condamnations)])
+    .interpolator(d3.interpolateBlues)
+  
+  // Ajouter les barres
+    svg.append('g')
+    .selectAll('rect')
+    .data(canton)
+    .enter()
+    .append('rect')
+    .attr('width', x.bandwidth())
+    .attr('height', d => y(0) - y(d.condamnations))
+    .attr('x', d => x(d.annee))
+    .attr('y', d => y(d.condamnations))
+    .style('fill', d => teinte(d.condamnations))
+  
+  // Ajouter les titres
+    svg.append('g')
+    .style('fill', 'white')
+    .attr('text-anchor', 'middle')
+    .attr('transform', `translate(${x.bandwidth() / 2}, 6)`)
+    .selectAll('text')
+    .data(canton)
+    .enter()
+    .append('text')
+    .attr('dy', '0.35em')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.condamnations))
+    .text(d => d.condamnations)
+  
+  // Ajouter l'axe horizontal
+    svg.append('g')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(d3.axisBottom(x))
+    .call(g => g.select('.domain').remove())
+  
+  // Ajouter l'axe vertical
+    svg.append('g')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select('.domain').remove())
 
-// Créer l'échelle horizontale (fonctions D3)
-const x = d3.scaleBand()
-            .domain(fruits.map(d => d.name))
-            .range([margin.left, width - margin.right])
-            .padding(0.1)
-            .round(true)
-
-// Créer l'échelle verticale (fonctions D3)
-const y = d3.scaleLinear()
-            .domain([0, d3.max(fruits, d => d.count)])
-            .range([height - margin.bottom - 5, margin.top])
-            .interpolate(d3.interpolateRound)
-
-const teinte = d3.scaleSequential()
-        .domain([0, d3.max(fruits, d => d.count)])
-        .interpolator(d3.interpolateBlues)
-
-// Ajouter les barres
-svg.append('g')
-   .selectAll('rect')
-     .data(fruits)
-     .enter()
-     .append('rect')
-     .attr('width', x.bandwidth())
-     .attr('height', d => y(0) - y(d.count))
-     .attr('x', d => x(d.name))
-     .attr('y', d => y(d.count))
-     .style('fill', d => d.color)
-
-// Ajouter les titres
-svg.append('g')
-   .style('fill', 'white')
-   .attr('text-anchor', 'middle')
-   .attr('transform', `translate(${x.bandwidth() / 2}, 6)`)
-   .selectAll('text')
-     .data(fruits)
-     .enter()
-     .append('text')
-     .attr('dy', '0.35em')
-     .attr('x', d => x(d.name))
-     .attr('y', d => y(d.count))
-     .text(d => d.count)
-
-// Ajouter l'axe horizontal
-svg.append('g')
-   .attr('transform', `translate(0, ${height - margin.bottom})`)
-   .call(d3.axisBottom(x))
-   .call(g => g.select('.domain').remove())
-
-// Ajouter l'axe vertical
-svg.append('g')
-   .attr('transform', `translate(${margin.left}, 0)`)
-   .call(d3.axisLeft(y))
-   .call(g => g.select('.domain').remove())
+});
