51e227abdd921e2711ad0eaf809696bf16f7b30a
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-01 14:30:04 +0200

    bar chart modifs

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,224 +1,74 @@
-// peut etre à enlever: data = d3.csvParse(await FileAttachment("Donnees CP.csv").text(), d3.autoType)
+// Paramètres de la visualisation
+const width = 600;
+const height = 300;
+const margin = { top: 20, right: 0, bottom: 20, left: 20 };
 
-d3.dsv(';', "data/Donnees CP.csv", function (d) {
-  return {
-    annee : d.Year
-    canton : d.Canton
-    condamnations adultes : d.CondamnationsAdultes
-  }
-}).then(function(data) {
-    
-  chart = {
-    // on a enlevé le "replay;" ici
-  
-    const : svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height]),
-        // min 18 video - peut etre ajouter svg.main
-  
-    const : updateBars = bars(svg),
-    const : updateAxis = axis(svg),
-    const : updateLabels = labels(svg),
-    const : updateTicker = ticker(svg),
-  
-    yield svg.node(),
-  
-    for (const keyframe of keyframes) {
-      const transition = svg.transition()
-          .duration(duration)
-          .ease(d3.easeLinear);
-  
-      // Extract the top bar’s value.
-      x.domain([0, keyframe[1][0].value]);
-  
-      updateAxis(keyframe, transition);
-      updateBars(keyframe, transition);
-      updateLabels(keyframe, transition);
-      updateTicker(keyframe, transition);
-  
-      invalidation.then(() => svg.interrupt());
-      await transition.end();
-    }
-  }
-  
-  
-  duration = 250
-  
-  n = 26
-  
-  Cantons = new Set(data.map(d => d.Canton))
-  
-  Yearvalues = Array.from(d3.rollup(data, ([d]) => d.CondamnationsAdultes, d => +d.Year, d => d.Canton))
-    .map(([Year, data]) => [new Year(Year), data])
-    .sort(([a], [b]) => d3.ascending(a, b))
-  
-  
-  function rank(CondamnationsAdultes) {
-      const data = Array.from(Cantons, Canton => ({Canton, CondamnationsAdultes: value(Canton)}));
-      data.sort((a, b) => d3.descending(a.CondamnationsAdultes, b.CondamnationsAdultes));
-      for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
-      return data;
-    }
-  
-  
-  k = 10
-  
-      
-  keyframes = {
-    const keyframes = [],
-    let ka, a, kb, b;
-    for ([[ka, a], [kb, b]] of d3.pairs(Yearvalues)) {
-      for (let i = 0; i < k; ++i) {
-        const t = i / k;
-        keyframes.push([
-          new Date(ka * (1 - t) + kb * t),
-          rank(Canton => (a.get(Canton) || 0) * (1 - t) + (b.get(Canton) || 0) * t)
-        ]);
-      }
-    }
-    keyframes.push([new Date(kb), rank(Canton => b.get(Canton) || 0)]);
-    return keyframes;
-  }
-  
-  Cantonframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.Canton)
-  
-  prev = new Map(Cantonframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
-  
-  next = new Map(Cantonframes.flatMap(([, data]) => d3.pairs(data)))
-  
-  
-  function bars(svg) {
-      let bar = svg.append("g")
-          .attr("fill-opacity", 0.6)
-        .selectAll("rect");
-    
-      return ([Year, data], transition) => bar = bar
-        .data(data.slice(0, n), d => d.Canton)
-        .join(
-          enter => enter.append("rect")
-            .attr("fill", color)
-            .attr("height", y.bandwidth())
-            .attr("x", x(0))
-            .attr("y", d => y((prev.get(d) || d).rank))
-            .attr("width", d => x((prev.get(d) || d).CondamnationsAdultes) - x(0)),
-          update => update,
-          exit => exit.transition(transition).remove()
-            .attr("y", d => y((next.get(d) || d).rank))
-            .attr("width", d => x((next.get(d) || d).CondamnationsAdultes) - x(0))
-        )
-        .call(bar => bar.transition(transition)
-          .attr("y", d => y(d.rank))
-          .attr("width", d => x(d.CondamnationsAdultes) - x(0)));
-    }
-  
-  
-  function labels(svg) {
-      let label = svg.append("g")
-          .style("font", "bold 12px var(--sans-serif)")
-          .style("font-variant-numeric", "tabular-nums")
-          .attr("text-anchor", "end")
-        .selectAll("text");
-    
-      return ([Year, data], transition) => label = label
-        .data(data.slice(0, n), d => d.Canton)
-        .join(
-          enter => enter.append("text")
-            .attr("transform", d => `translate(${x((prev.get(d) || d).CondamnationsAdultes)},${y((prev.get(d) || d).rank)})`)
-            .attr("y", y.bandwidth() / 2)
-            .attr("x", -6)
-            .attr("dy", "-0.25em")
-            .text(d => d.Canton)
-            .call(text => text.append("tspan")
-              .attr("fill-opacity", 0.7)
-              .attr("font-weight", "normal")
-              .attr("x", -6)
-              .attr("dy", "1.15em")),
-          update => update,
-          exit => exit.transition(transition).remove()
-            .attr("transform", d => `translate(${x((next.get(d) || d).CondamnationsAdultes)},${y((next.get(d) || d).rank)})`)
-            .call(g => g.select("tspan").tween("text", d => textTween(d.CondamnationsAdultes, (next.get(d) || d).CondamnationsAdultes)))
-        )
-        .call(bar => bar.transition(transition)
-          .attr("transform", d => `translate(${x(d.CondamnationsAdultes)},${y(d.rank)})`)
-          .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).CondamnationsAdultes, d.CondamnationsAdultes))));
-    }
-  
-  
-  
-  function textTween(a, b) {
-      const i = d3.interpolateNumber(a, b);
-      return function(t) {
-        this.textContent = formatNumber(i(t));
-      };
-    }
-  
-  
-  
-  formatNumber = d3.format(",d")
-  
-  
-  
-  function axis(svg) {
-      const g = svg.append("g")
-          .attr("transform", `translate(0,${margin.top})`);
-    
-      const axis = d3.axisTop(x)
-          .ticks(width / 160)
-          .tickSizeOuter(0)
-          .tickSizeInner(-barSize * (n + y.padding()));
-    
-      return (_, transition) => {
-        g.transition(transition).call(axis);
-        g.select(".tick:first-of-type text").remove();
-        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
-        g.select(".domain").remove();
-      };
-    }
-  
-  
-  
-  function ticker(svg) {
-      const now = svg.append("text")
-          .style("font", `bold ${barSize}px var(--sans-serif)`)
-          .style("font-variant-numeric", "tabular-nums")
-          .attr("text-anchor", "end")
-          .attr("x", width - 6)
-          .attr("y", margin.top + barSize * (n - 0.45))
-          .attr("dy", "0.32em")
-          .text(formatDate(keyframes[0][0]));
-    
-      return ([Year], transition) => {
-        transition.end().then(() => now.text(formatDate(Year)));
-      };
-    }
-  
-  
-  
-  formatDate = d3.utcFormat("%Y")
-  
-  
-  
-  color = {
-    const scale = d3.scaleOrdinal(d3.schemeTableau10),
-    }
-    return d => scale(d.Canton);
-  }
-  
-  
-  x = d3.scaleLinear([0, 1], [margin.left, width - margin.right])
-  
-  
-  y = d3.scaleBand()
-      .domain(d3.range(n + 1))
-      .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
-      .padding(0.1)
-  
-  
-  height = margin.top + barSize * n + margin.bottom
-  
-  barSize = 48
-  
-  margin = ({top: 16, right: 6, bottom: 6, left: 0}) 
-  
-  d3 = require("d3@5", "d3-array@2")
-});
+// Données
+const fruits = [
+  { name: "🍊", count: 21 , color: 'orange' },
+  { name: "🍇", count: 13 , color: 'blueviolet' },
+  { name: "🍏", count: 8 , color: 'greenyellow' },
+  { name: "🍌", count: 5 , color: 'yellow' },
+  { name: "🍐", count: 3 , color: 'yellowgreen' },
+  { name: "🍋", count: 2 , color: 'gold' },
+  { name: "🍎", count: 1 , color: 'red' },
+  { name: "🍉", count: 1 , color: 'orangered' }
+];
 
+// Créer l'élément SVG et le configurer
+const svg = d3.select('body')
+              .append('svg')
+              .attr('width', width)
+              .attr('height', height)
+              .attr('style', 'font: 10px sans-serif')
+
+// Créer l'échelle horizontale (fonctions D3)
+const x = d3.scaleBand()
+            .domain(fruits.map(d => d.name))
+            .range([margin.left, width - margin.right])
+            .padding(0.1)
+            .round(true)
+
+// Créer l'échelle verticale (fonctions D3)
+const y = d3.scaleLinear()
+            .domain([0, d3.max(fruits, d => d.count)])
+            .range([height - margin.bottom - 5, margin.top])
+            .interpolate(d3.interpolateRound)
+
+// Ajouter les barres
+svg.append('g')
+   .selectAll('rect')
+     .data(fruits)
+     .enter()
+     .append('rect')
+     .attr('width', x.bandwidth())
+     .attr('height', d => y(0) - y(d.count))
+     .attr('x', d => x(d.name))
+     .attr('y', d => y(d.count))
+     .style('fill', d => d.color)
+
+// Ajouter les titres
+svg.append('g')
+   .style('fill', 'white')
+   .attr('text-anchor', 'middle')
+   .attr('transform', `translate(${x.bandwidth() / 2}, 6)`)
+   .selectAll('text')
+     .data(fruits)
+     .enter()
+     .append('text')
+     .attr('dy', '0.35em')
+     .attr('x', d => x(d.name))
+     .attr('y', d => y(d.count))
+     .text(d => d.count)
+
+// Ajouter l'axe horizontal
+svg.append('g')
+   .attr('transform', `translate(0, ${height - margin.bottom})`)
+   .call(d3.axisBottom(x))
+   .call(g => g.select('.domain').remove())
+
+// Ajouter l'axe vertical
+svg.append('g')
+   .attr('transform', `translate(${margin.left}, 0)`)
+   .call(d3.axisLeft(y))
+   .call(g => g.select('.domain').remove())
