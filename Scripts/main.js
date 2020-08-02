1c202caffb1899eacac789f8442af941803c39de
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-02 11:01:24 +0200

    ca marche!

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,17 +1,22 @@
 // Paramètres de la visualisation
-const width = 600;
+const width = 2000;
 const height = 300;
 const margin = { top: 20, right: 0, bottom: 20, left: 20 };
 
 d3.dsv(";", "Data/Donnees CP.csv", function (d) {
     return {
-        lieu: d.Canton,
+        canton: d.Canton,
         annee: d.Year,
         condamnations: d.CondamnationsAdultes
     };
 }).then(function(data) {
-
+    
+    const canton = data.filter(d => d.canton === "NE");
+    
+    console.log(canton)
   
+    // min 45.15 S9 - notre graphique disparait avec un NaN Parsing Height message
+
   // Créer l'élément SVG et le configurer
     const svg = d3.select('.main')
     .append('svg')
@@ -21,25 +26,25 @@ d3.dsv(";", "Data/Donnees CP.csv", function (d) {
   
   // Créer l'échelle horizontale (fonctions D3)
     const x = d3.scaleBand()
-    .domain(lieu.map(d => d.annee))
+    .domain(canton.map(d => d.annee))
     .range([margin.left, width - margin.right])
     .padding(0.1)
     .round(true)
   
   // Créer l'échelle verticale (fonctions D3)
     const y = d3.scaleLinear()
-    .domain([0, d3.max(lieu, d => d.condamnations)])
+    .domain([0, d3.max(canton, d => d.condamnations)])
     .range([height - margin.bottom - 5, margin.top])
     .interpolate(d3.interpolateRound)
   
     const teinte = d3.scaleSequential()
-    .domain([0, d3.max(lieu, d => d.condamnations)])
+    .domain([0, d3.max(canton, d => d.condamnations)])
     .interpolator(d3.interpolateBlues)
   
   // Ajouter les barres
     svg.append('g')
     .selectAll('rect')
-    .data(lieu)
+    .data(canton)
     .enter()
     .append('rect')
     .attr('width', x.bandwidth())
@@ -54,7 +59,7 @@ d3.dsv(";", "Data/Donnees CP.csv", function (d) {
     .attr('text-anchor', 'middle')
     .attr('transform', `translate(${x.bandwidth() / 2}, 6)`)
     .selectAll('text')
-    .data(lieu)
+    .data(canton)
     .enter()
     .append('text')
     .attr('dy', '0.35em')
