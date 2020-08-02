b3a33be091abbde3efd6661cd6c10c4737fe47eb
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-02 13:44:48 +0200

    changement echelles

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,113 +1,82 @@
 // Paramètres de la visualisation
 const width = 2000;
 const height = 300;
-const margin = { top: 20, right: 0, bottom: 20, left: 50 };
+const margin = { top: 120, right: 0, bottom: 20, left: 50 };
 
-// données complètes
-let cpData;
-
-let condBars;
-let condTitles;
-// échelles D3
-let condScaleX, condScaleY;
-let condColorScale;
-
-function setup() {
-    loadData();
-    setupCondamnationsPenales();
-}
-
-function loadData() {
-    d3.dsv(";", "Data/Donnees CP.csv", function (d) {
-        return {
-            canton: d.Canton,
-            annee: d.Year,
-            condamnations: d.CondamnationsAdultes
-        }
-    }).then(onDataLoaded);
-}
-
-function onDataLoaded(data) {
-    cpData = data;
-    graphCondamnationsPenales("ZG");
-}
-
-function setupCondamnationsPenales() {
-    const minJuge = 0;
-    const maxJuge = 18000;
+d3.dsv(";", "Data/Donnees CP.csv", function (d) {
+    return {
+        canton: d.Canton,
+        annee: d.Year,
+        condamnations: d.CondamnationsAdultes
+    };
+}).then(function(data) {
     
-    const svg = d3.select('.cond')
+    const canton = data.filter(d => d.canton === "ZH");
+
+
+  // Créer l'élément SVG et le configurer
+    const svg = d3.select('.main')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
-    .attr('style', 'font: 10px sans-serif');
-
-
-    condScaleX = d3.scaleBand()
-    .domain([1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017])
+    .attr('style', 'font: 10px sans-serif')
+  
+  // Créer l'échelle horizontale (fonctions D3)
+    const x = d3.scaleBand()
+    .domain(canton.map(d => d.annee))
     .range([margin.left, width - margin.right])
     .padding(0.1)
     .round(true)
   
-    condScaleY = d3.scaleLinear()
-    .domain([minJuge, maxJuge])
+  // Créer l'échelle verticale (fonctions D3)
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(canton, d => d.condamnations)])
     .range([height - margin.bottom - 5, margin.top])
     .interpolate(d3.interpolateRound)
-    
-    condColorScale = d3.scaleSequential()
-    .domain([minJuge, maxJuge])
-    .interpolator(d3.interpolateBlues)
-    
-    condBars = svg.append ("g");
-    condTitles = svg.append ("g")
-        .style ("fill", "white")
-        .attr ("text-anchor", "middle")
-        .attr ("transform", "translate(${condScaleX.bandwidth() / 2}, 6)");
-
-    
-// Ajouter l'axe horizontal
-    svg.append('g')
-        .attr('transform', `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(condScaleX))
-        .call(g => g.select('.domain').remove());
   
-// Ajouter l'axe vertical
-    svg.append('g')
-        .attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(condScaleY))
-        .call(g => g.select('.domain').remove())
-}
-
-
-function graphCondamnationsPenales(canton, annee) {
-
-    const data = cpData.filter(d => d.canton === canton && d.Year === annee);
-    // peut etre mettre year au lieu d'annee
-
+    const teinte = d3.scaleSequential()
+    .domain([0, d3.max(canton, d => d.condamnations)])
+    .interpolator(d3.interpolateBlues)
+  
   // Ajouter les barres
-    condBars.selectAll('rect')
-        .data(data)
-        .join('rect')
-            .attr('width', condScaleX.bandwidth())
-            .attr('height', d => condScaleY(0) - condScaleY(d.condamnations))
-            .attr('x', d => condScaleX(d.annee))
-            .attr('y', d => condScaleY(d.condamnations))
-            .style('fill', d => condColorScale(d.condamnations));
+    svg.append('g')
+    .selectAll('rect')
+    .data(canton)
+    .enter()
+    .append('rect')
+    .attr('width', x.bandwidth())
+    .attr('height', d => y(0) - y(d.condamnations))
+    .attr('x', d => x(d.annee))
+    .attr('y', d => y(d.condamnations))
+    .style('fill', d => teinte(d.condamnations))
   
   // Ajouter les titres
-    condTitles.selectAll('text')
-        .data(data)
-        .join('text')
-            .attr('dy', '0.35em')
-            .attr('x', d => condScaleX(d.annee))
-            .attr('y', d => condScaleY(d.condamnations))
-            .text(d => d.condamnations)
-    
-}
-      
+    svg.append('g')
+    .style('fill', 'white')
+    .attr('text-anchor', 'middle')
+    .attr('transform', `translate(${x.bandwidth() / 2}, 6)`)
+    .selectAll('text')
+    .data(canton)
+    .enter()
+    .append('text')
+    .attr('dy', '0.35em')
+    .attr('x', d => x(d.annee))
+    .attr('y', d => y(d.condamnations))
+    .text(d => d.condamnations)
+  
+  // Ajouter l'axe horizontal
+    svg.append('g')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(d3.axisBottom(x))
+    .call(g => g.select('.domain').remove())
+  
+  // Ajouter l'axe vertical
+    svg.append('g')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select('.domain').remove())
 
-//lance le script
-setup();
+});
 
 // vérifier les chiffres sur l'echelle verticale
 // vidéo 10 GitHub Pages à faire - sinon, lui signaler que on a pas fait cet étape puisque c'est payant
