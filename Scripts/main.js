51d2fb2345bbf98e4d5ca104e09504a677a2a212
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-25 15:12:06 +0200

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -166,6 +166,7 @@ function graphCondamnationsPenales() {
 }
 // c'est la fonction de dessin et de mise à jour
 
+
 setup();
 
 
@@ -173,7 +174,7 @@ setup();
 // questions:
             //3) Faire 2ème graphique en rajoutant un SVG, ajouter la fonctionalité de l'année pour voir les résultats par année pour tous les cantons ou bien faire en sorte que la glissiere par année montre par défaut tous les cantons
             //7) ajouter les icônes des drapeaux des cantons (download via wiki, png créer fichier data avec les drapeaux append.image(svg)) + améliorer mise en page
-            //8) Mettre répositoire sur public pour afficher les pages
+            //8) Déterminer pourquoi le lien du repositoire depuis Github, n'affiche pas la même chose que depuis VSC
             //9) Améliorer Readme (selon mail)
             
            
