fa4a4ae5fcac62fe5e0af9b57efcbb2f728a00d0
KVVisDon <63305173+KVVisDon@users.noreply.github.com>
2020-08-31 13:59:36 +0200

    ajouts image au main.css

--- Scripts/main.js
+++ Scripts/main.js
@@ -178,8 +178,7 @@ setup();
 
             //1) Faire 2ème graphique en rajoutant un SVG, ajouter la fonctionalité de l'année pour voir les résultats par année pour tous les cantons ou bien faire en sorte que la glissière par année montre par défaut tous les cantons
             //2) ajouter les icônes des drapeaux des cantons (download via wiki, png créer fichier data avec les drapeaux append.image(svg)) + améliorer mise en page
-            //3) Mettre répositoire sur public pour afficher les pages
-            //4) Améliorer Readme (selon mail)
+            //3) Améliorer Readme (selon mail)
 
             
            
